fe867058b665fbe9cf763d74eba7a731ebbcc213
Lumbani <48856311+lumbapoke@users.noreply.github.com>
2020-11-23 17:29:32 +0200

    Update stage1.py

--- Desktop/CoffeeMachine/stage1.py
+++ Desktop/CoffeeMachine/stage1.py
@@ -1,7 +1,7 @@
-print("
+print("""
 Grinding coffee beans
 Boiling water
 Mixing boiled water with crushed coffee beans
 Pouring coffee into the cup
 Pouring some milk into the cup
-Coffee is ready!")
+Coffee is ready!""")
